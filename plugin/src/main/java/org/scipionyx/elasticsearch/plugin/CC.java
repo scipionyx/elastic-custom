a9f4d970965e0e669873c4ac31db2ead62170ca6
Renato Mendes <renato.mendes.1123@gmail.com>
2018-10-19 13:16:28 -0400

    New project

---
+++ plugin/src/main/java/org/scipionyx/elasticsearch/plugin/CC.java
@@ -0,0 +1,16 @@
+package org.scipionyx.elasticsearch.plugin;
+
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.plugins.SearchPlugin;
+
+public class CC extends Plugin implements SearchPlugin {
+
+//    @Override
+//    public List<RescorerSpec<?>> getRescorers() {
+//        return singletonList(
+//                new RescorerSpec<>(ExampleRescoreBuilder.NAME, ExampleRescoreBuilder::new, ExampleRescoreBuilder::fromXContent));
+//    }
+
+
+
+}
